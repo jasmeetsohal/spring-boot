78632a10a3203be88bb4253675412e0aeaec202f
upender <uksharma21@gmail.com>
2019-10-23 12:57:03 +0530

    Modified application class.

--- src/main/java/com/hacktober/springboot/ApplicationMain.java
+++ src/main/java/com/hacktober/springboot/ApplicationMain.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApplicationMain {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		System.out.println("Starting the application......");
+		SpringApplication.run(ApplicationMain.class, args);
+		
 	}
 
 }
